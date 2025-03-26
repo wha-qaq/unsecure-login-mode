bfd5ef8d1c062be2355e1fe113b6f19762c2920c
aaa <163373067+wha-qaq@users.noreply.github.com>
2025-03-26 01:15:02 +0000

    Hashing Passwords

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const path = require('path');
+const bycrpt = require('bcrypt');
 
 const app = express();
 const db = new sqlite3.Database('database.db');
+
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,10 +27,12 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { username, password } = req.body;
-    const sql = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
-    db.run(sql, (err) => {
-        if (err) return res.send('Error: ' + err.message);
-        res.send('User registered! <a href="/login">Login here</a>');
+    bycrpt.hash(password, saltRounds).then(hashed => {
+        const sql = `INSERT INTO users (username, password) VALUES ('${username}', '${hashed}')`;
+        db.run(sql, (err) => {
+            if (err) return res.send('Error: ' + err.message);
+            res.send('User registered! <a href="/login">Login here</a>');
+        });
     });
 });
 
@@ -37,12 +42,15 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    const sql = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+    const sql = `SELECT * FROM users WHERE username = '${username}'`; // Bycrpt will handle the password
     db.get(sql, (err, row) => {
         if (err) return res.send('Error: ' + err.message);
-        if (row) return res.send(`Welcome, ${username}!`);
-        res.send('Login failed. <a href="/login">Try again</a>');
-    });
+        if (!row) return res.send('Login failed. <a href="/login">Try again</a>');
+        bycrpt.compare(password, row.password).then(success => {
+            if (!success) return res.send('Login failed. <a href="/login">Try again</a>');
+            res.send(`Welcome, ${username}!`);
+        });
+    });  
 });
 
 app.listen(3000, () => {
